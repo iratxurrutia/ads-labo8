566b9d987e4001d083810607d6d413fe0cc8480b
iratxurrutia <146710848+iratxurrutia@users.noreply.github.com>
2025-12-08 22:14:33 +0100

    Change greeting message in hello route

--- app/app.py
+++ app/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hola! Este sitio se ha visitado {} veces.\n'.format(count)
+    return 'Saludos desde CI/CD! Este sitio se ha visitado {} veces.\n'.format(count)
